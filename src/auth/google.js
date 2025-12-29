951de96aaf3b2ea0977e74757de2589310855260
rdhforeclosureconqueror-sys <rdhforeclosureconqueror@gmail.com>
2025-12-28 21:56:53 -0600

    Implement Google OAuth authentication with Passport

---
+++ src/auth/google.js
@@ -0,0 +1,28 @@
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      // Later: save or lookup user in DB
+      return done(null, {
+        googleId: profile.id,
+        email: profile.emails?.[0]?.value,
+        name: profile.displayName
+      });
+    }
+  )
+);
